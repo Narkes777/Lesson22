cca72dc6d381bc121e833e91a32948ab2062c4b6
Nick Selin <103569863+NickFlabel@users.noreply.github.com>
2024-02-15 21:12:36 +0600

    15.02

--- lesson_08_01/new_app/urls.py
+++ lesson_08_01/new_app/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('posts/', views.PostList.as_view(), name='post_list'),
+    path('posts/', views.post_list, name='post_list'),
     path('posts/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
     path('posts/create/', views.PostCreate.as_view(), name='post_create'),
     path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='post_update'),
--- lesson_08_01/new_app/views.py
+++ lesson_08_01/new_app/views.py
@@ -3,6 +3,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
+from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from .models import Post
 
@@ -10,6 +12,18 @@ from .models import Post
 class PostList(ListView):
     model = Post
     # context_object_name = 'object_list'
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 2)
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 1
+    page = paginator.get_page(page_num)
+    context = {"object_list": page.object_list, "page": page}
+    return render(request, "new_app/post_list.html", context)
 
 
 class PostDetail(DetailView):
@@ -38,16 +52,20 @@ class PostDelete(DeleteView):
 class MainPage(TemplateView):
     template_name = 'new_app/about.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        b = [1, 1, 1, 2, 3, 3, 3, 4, 4, 5]
-        c = Post.objects.last()
-        condition = 10
-        context['b'] = b
-        context['post'] = c
-        context['condition'] = condition
-        # new_context = {'var': a}
-        # context.update(new_context)
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     b = [1, 1, 1, 2, 3, 3, 3, 4, 4, 5]
+    #     list_of_booleans = [True, False, True, None]
+    #     c = Post.objects.last()
+    #     condition = 10
+    #     number = 34.123131
+    #     context['b'] = b
+    #     context['post'] = c
+    #     context['condition'] = condition
+    #     context['boolean_list'] = list_of_booleans
+    #     context['number'] = number
+    #     # new_context = {'var': a}
+    #     # context.update(new_context)
+    #     return context
 
 
